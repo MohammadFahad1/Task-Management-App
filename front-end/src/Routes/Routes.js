22897856b3cbcc76c0ff7489ff3fb49f3c0f2da2
MohammadFahad1 <fahad4bangladesh@gmail.com>
2022-12-28 22:44:17 +0600

    Basic File Setup done

---
+++ front-end/src/Routes/Routes.js
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <h1>Welcome to Home</h1>
+            }
+        ]
+    }
+])
+
+export default router;
